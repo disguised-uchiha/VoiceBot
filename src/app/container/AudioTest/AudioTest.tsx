e7b3554e3f3e365c3e04c48ad0a11b613cabfc17
disguised-uchiha <harsh.boricha2015@gmail.com>
2022-04-28 11:14:27 +0530

    fix: removed .env

--- src/app/container/AudioTest/AudioTest.tsx
+++ src/app/container/AudioTest/AudioTest.tsx
@@ -1,20 +1,11 @@
-import { useState } from "react";
+import { Button } from "@mui/material";
 import styles from "./AudioTest.module.scss";
 
 const AudioTest = () => {
-  const [lang, setLang] = useState("en");
-
   return (
-    <div onClick={() => setLang(lang === "en" ? "hi" : "en")} className={styles.container}>AudioTest
-      <div dangerouslySetInnerHTML={{
-        __html: `
-   <df-messenger
-     intent="WELCOME"
-     chat-title="FirstTest"
-     agent-id="xxxxxxxxxxx"
-     language-code=${lang}>
-   </df-messenger>
-` }} /></div>
+    <div>
+      <Button>Speak</Button>
+    </div>
   )
 }
 
