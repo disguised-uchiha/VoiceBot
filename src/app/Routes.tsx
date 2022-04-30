ecd004819d658c6fffe0bfaafafe41579a2a7565
disguised-uchiha <harsh.boricha2015@gmail.com>
2022-04-30 17:51:46 +0530

    feat: chatui

--- src/app/Routes.tsx
+++ src/app/Routes.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
-import AudioTest from './container/AudioTest/AudioTest'
+import AudioTest from './containers/AudioTest/AudioTest'
+import Chatbot from './containers/Chatbot/Chatbot'
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<AudioTest />} />
+      <Route path="/" element={<Chatbot />} />
     </Routes>
   )
 }
